69f82221f4ac0594100f7807dde445d3d4881e92
Dmitriy Balashov <mockedcat@gmail.com>
2019-03-03 12:43:24 +0300

    parallax background feature was implemented

--- main.lua
+++ main.lua
@@ -3,13 +3,19 @@ local push = require("libs/push")
 gameWidth = 512
 gameHeight = 288
 
--- size of our actual window
 local windowWidth = 1280
 local windowHeight = 720
 
 local background = love.graphics.newImage('assets/images/background.png')
-local ground = love.graphics.newImage('assets/images/ground.png')
+local backgroundScroll = 0
+local backgroundScrollSpeed = 30
+local backgroundLoopingPoint = 413
 
+local ground = love.graphics.newImage('assets/images/ground.png')
+local groundScroll = 0
+local groundScrollSpeed = 60
+
+-- 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -32,6 +38,12 @@ function love.resize(w, h)
   push:resize(w, h)
 end
 
+function love.update(dt)
+  backgroundScroll = (backgroundScroll + (backgroundScrollSpeed * dt)) % backgroundLoopingPoint
+
+  groundScroll = (groundScroll + (groundScrollSpeed * dt)) % gameWidth
+end
+
 function love.keypressed(key)
   if key == 'escape' then
     love.event.quit()
@@ -41,8 +53,8 @@ end
 function love.draw()
   push:start()
 
-  love.graphics.draw(background, 0, 0)
-  love.graphics.draw(ground, 0, gameHeight - 16)
+  love.graphics.draw(background, -backgroundScroll, 0)
+  love.graphics.draw(ground, -groundScroll, gameHeight - 16)
 
   push:finish()
 end
